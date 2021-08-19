ccd9888b8a0655524400b11a4df6d3722a7f3805
孤影浪人 <1315723390@qq.com>
2021-08-19 17:48:27 +0800

    解决不同模式vue-i18编译问题

--- vite.config.js
+++ vite.config.js
@@ -4,37 +4,41 @@ const { resolve } = require("path");
 /**
  * @type {import('vite').UserConfig}
  */
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-      "/@": resolve(__dirname, "src"),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',//解决dev运行警告You are running the esm-bundler build of vue-i18n. It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
-    },
-  },
-  base: "./",
-  server: {
-    host: "127.0.0.1",
-    port: 8086,
-    open: true,
-    // 反向代理
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:80/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command, mode }) => {
+  let vueI18n = {};
+  if (command === "serve") {
+    vueI18n["vue-i18n"] = "vue-i18n/dist/vue-i18n.cjs.js"; //解决dev运行警告You are running the esm-bundler build of vue-i18n.
+  }
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "/@": resolve(__dirname, "src"),
+        ...vueI18n,
       },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/style/element-variables.scss";`,
-        //引用公共样式，使用vite搭建项目只安装sass即可，不需要安装node-sass,sass-loader
+    base: "./",
+    server: {
+      host: "127.0.0.1",
+      port: 8086,
+      open: true,
+      // 反向代理
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:80/",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/style/element-variables.scss";`,
+          //引用公共样式，使用vite搭建项目只安装sass即可，不需要安装node-sass,sass-loader
+        },
+      },
+    },
+  };
 });
